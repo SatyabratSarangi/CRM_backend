db978e02d50c756289426d72b0d5de0e6e5fc603
Satyabrat Sarangi <satyabratsarangi1211@gmail.com>
2026-07-09 23:24:56 +0530

    updated application properties

--- src/main/java/com/satya/CRM/CrmApplication.java
+++ src/main/java/com/satya/CRM/CrmApplication.java
@@ -2,11 +2,13 @@ package com.satya.CRM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class CrmApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(CrmApplication.class, args);
 	}
 
